e56f8f4e101d19faa600c51a870eb21faa27bf80
sine <qxlovezh@gmail.com>
2020-12-10 17:09:12 +0800

    20201210study01

--- src/How2J/Item.java
+++ src/How2J/Item.java
@@ -2,7 +2,19 @@ public class Item {
     String name;    //物品名称
     int price;  //物品价格
 
-    public void getprice(String name) {
-
+    public Item(String ItemName,int ItemPrice) {
+        name = ItemName;
+        price = ItemPrice;
+    }
+    public void Checkprice() {
+        System.out.println(name + "的价格是" + price);
+    }
+    public static void main(String[] args) {
+        Item Bloodbottle = new Item("血瓶",50);
+        Item Strawsandals = new Item("草鞋",300);
+        Item Longsword = new Item("长剑",350);
+        Bloodbottle.Checkprice();
+        Strawsandals.Checkprice();
+        Longsword.Checkprice();
     }
 }
